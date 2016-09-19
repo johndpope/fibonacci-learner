fac0af2a516d80d09ed232e806743bd86f048fc7
Adam Atkinson <adam@aatkinson.ca>
2016-09-19 19:44:51 -0400

    added more data, not getting substantially better

--- keras/digit_addition/fibonacci_lstm.py
+++ keras/digit_addition/fibonacci_lstm.py
@@ -36,8 +36,8 @@ MAX_DIGITS = 105
 N_FIB_TERMS = 400
 N_3_WINDOWS = N_FIB_TERMS - 3 + 1
 
-def int_to_digits (n):
-    digits = np.zeros(N_DIGITS)
+def int_to_digits (n, n_digits=MAX_DIGITS):
+    digits = np.zeros(n_digits)
     n_str = "{0:0.0f}".format(n)
     for i in xrange(len(n_str)):
         digits[N_DIGITS-1-i] = int(n_str[len(n_str)-1-i])
@@ -49,8 +49,8 @@ fibonacci_ref = pd.read_csv("fibonacci_sequence.csv", header=None)
 # Generate the dataset
 # Duplicate each sample, with t-1 and t-2 terms swapped 
 # TODO: does this have an affect?
-X_all = np.zeros([2*N_3_WINDOWS, MAX_DIGITS, 20], dtype=np.float64)
-Y_all = np.zeros([2*N_3_WINDOWS, MAX_DIGITS, 10], dtype=np.float64)
+X_fib_all = np.zeros([2*N_3_WINDOWS, MAX_DIGITS, 20], dtype=np.float64)
+Y_fib_all = np.zeros([2*N_3_WINDOWS, MAX_DIGITS, 10], dtype=np.float64)
 
 for i in xrange(N_3_WINDOWS):
 
@@ -61,28 +61,60 @@ for i in xrange(N_3_WINDOWS):
     # Fill digit of t-2 term (X)
     for j in xrange(len(first)):
         k = int(first[j])
-        X_all[i][j][k] = 1.0
-        X_all[i+N_3_WINDOWS][j][k+10] = 1.0
+        X_fib_all[i][j][k] = 1.0
+        X_fib_all[i+N_3_WINDOWS][j][k+10] = 1.0
 
     # Fill digit of t-1 term (X)
     for j in xrange(len(second)):
         k = int(second[j])
-        X_all[i][j][k+10] = 1.0
-        X_all[i+N_3_WINDOWS][j][k] = 1.0
+        X_fib_all[i][j][k+10] = 1.0
+        X_fib_all[i+N_3_WINDOWS][j][k] = 1.0
 
     # Fill digit of sum / t term (Y)
     for j in xrange(len(third)):
         k = int(third[j])
-        Y_all[i][j][k] = 1.0
-        Y_all[i+N_3_WINDOWS][j][k] = 1.0
+        Y_fib_all[i][j][k] = 1.0
+        Y_fib_all[i+N_3_WINDOWS][j][k] = 1.0
+
+# Generate extra data
+N_EXTRA_DATA_POINTS = 10000
+EXTRA_DATA_MAX = np.iinfo(np.int64).max / 2
+
+X_extra_all = np.zeros([N_EXTRA_DATA_POINTS, MAX_DIGITS,20])
+Y_extra_all = np.zeros([N_EXTRA_DATA_POINTS, MAX_DIGITS,10])
+
+for i in xrange(N_EXTRA_DATA_POINTS):
+
+    first_n = np.random.randint(0,EXTRA_DATA_MAX)
+    second_n = np.random.randint(0,EXTRA_DATA_MAX)
+    third_n = first_n + second_n
+
+    first = "{0:0.0f}".format(first_n)[::-1]
+    second = "{0:0.0f}".format(second_n)[::-1]
+    third = "{0:0.0f}".format(third_n)[::-1]
+
+    # Fill digit of t-2 term (X)
+    for j in xrange(len(first)):
+        k = int(first[j])
+        X_extra_all[i][j][k] = 1.0
+
+    # Fill digit of t-1 term (X)
+    for j in xrange(len(second)):
+        k = int(second[j])
+        X_extra_all[i][j][k+10] = 1.0
+
+    # Fill digit of sum / t term (Y)
+    for j in xrange(len(third)):
+        k = int(third[j])
+        Y_extra_all[i][j][k] = 1.0
 
 # Create training and test sets by randomly sampling the whole data set
 test_split = 0.2
-test_split_mask = np.random.rand(X_all.shape[0]) < (1-test_split)
-X_train = X_all[test_split_mask]
-Y_train = Y_all[test_split_mask]
-X_test = X_all[~test_split_mask]
-Y_test = Y_all[~test_split_mask]
+#test_split_mask = np.random.rand(X_extra_all.shape[0]) < (1-test_split)
+#X_train = X_extra_all[test_split_mask]
+#Y_train = Y_extra_all[test_split_mask]
+#X_test = X_extra_all[~test_split_mask]
+#Y_test = Y_extra_all[~test_split_mask]
 
 # Define and train the model
 model = Sequential()
@@ -96,7 +128,7 @@ opt = Adam(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 print model.summary()
 
-model.fit(X_train, Y_train, nb_epoch=100, batch_size=100)
+model.fit(X_extra_all, Y_extra_all, nb_epoch=50, batch_size=500)
 
-scores = model.evaluate(X_test, Y_test, verbose=0)
+scores = model.evaluate(X_fib_all, Y_fib_all, verbose=0)
 print "Accuracy: %.2f%%" % (scores[1]*100)
